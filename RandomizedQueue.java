01bb162c831a993a4b5f139eadbe8e303efcc9f5
Piwe13 <cgonzaSLC@gmail.com>
2016-10-06 12:03:46 -0600

    init RandominizedQ

---
+++ RandomizedQueue.java
@@ -0,0 +1,40 @@
+package deques;
+
+public class RandomizedQueue {
+	public RandomizedQueue(){
+		// construct an empty randomized queue
+	}
+	/*
+	public boolean isEmpty(){
+		// is the queue empty?
+		return ;
+	}
+	
+	public int size(){
+		// return the number of items on the queue
+		return ;
+	}
+	
+	public void enqueue(Item item){
+		// add the item
+	}
+	
+	public Item dequeue(){
+		// delete and return a random item
+		return ;
+	}
+	
+	public Item sample(){
+		// return (but do not delete) a random item
+		return ;
+	}
+	
+	public Iterator<Item> iterator(){
+		// return an independent iterator over items in random order
+		return ;
+	}
+	*/
+	public static void main(String[] args){
+		// unit testing
+	}
+}
